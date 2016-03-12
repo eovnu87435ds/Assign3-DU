ca54d12e4ab14784ed33b28fca898f38764f95ea
Michael Dornisch <michaeldornisch@Michaels-MBP.home>
2016-03-12 12:24:54 -0500

    grading files created

---
+++ 1/openhash.py
@@ -0,0 +1,4 @@
+#Michael Dornisch
+#Implement Dictionary ADT (4.5-4.8) with open hash tables (fig. 4.12) for problem 1
+#Verify the average number of probes required to make either a deletion or an insertion is O(1+a).
+#Find best numerical constants for deletion and insertion.
